4478847873aeed79b5ac6f0d08cf2d4ac0abdb52
webimpress <contact@webimpress.com>
2017-12-08 08:32:01 +0000

    Added declare strict types

--- src/Exception/SessionSegmentConflictException.php
+++ src/Exception/SessionSegmentConflictException.php
@@ -5,6 +5,8 @@
  * @license   https://github.com/zendframework/zend-expressive-session/blob/master/LICENSE.md New BSD License
  */
 
+declare(strict_types=1);
+
 namespace Zend\Expressive\Session\Exception;
 
 use RuntimeException;
