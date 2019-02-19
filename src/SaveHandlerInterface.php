e846379b0f17a5b9aada16df3c3c1535ac74bc7f
Oleg Kasyanov <dev@mobicms.net>
2019-02-19 13:40:50 +0400

    Added SaveHandlerInterface

---
+++ src/SaveHandlerInterface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @see       https://github.com/zendframework/zend-expressive-session for the canonical source repository
+ * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (https://www.zend.com)
+ * @license   https://github.com/zendframework/zend-expressive-session/blob/master/LICENSE.md New BSD License
+ */
+
+namespace Zend\Expressive\Session;
+
+use SessionHandlerInterface;
+
+/**
+ * SaveHandler Interface
+ *
+ * @see        http://php.net/session_set_save_handler
+ */
+interface SaveHandlerInterface extends SessionHandlerInterface
+{
+}
